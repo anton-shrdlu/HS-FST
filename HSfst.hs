14d52a8117ccef933ee77c8b031841ec08e44e6a
anton-shrdlu <mail@antonhampe.de>
2020-06-22 18:01:25 +0200

    improved indentation

--- HSfst.hs
+++ HSfst.hs
@@ -3,20 +3,20 @@
 -- Gen and all Constrains are FSTs, the evaluation mechanism is a regular old function.
 
 module HSfst
-     ( State
-     , GEN
-     , Sequence(SQ)
-     , Constraint
-     , CON
-     , mkGen
-     , mkMax
-     , mkDep
-     , mkId
-     , mkPreds
-     , mkMark
-     , converge
-     , converge'
-     ) where
+    ( State
+    , GEN
+    , Sequence(SQ)
+    , Constraint
+    , CON
+    , mkGen
+    , mkMax
+    , mkDep
+    , mkId
+    , mkPreds
+    , mkMark
+    , converge
+    , converge'
+    ) where
 
 import Data.Maybe (mapMaybe, fromJust)
 import Data.List (nub, inits)
@@ -35,17 +35,17 @@ type GEN a = NFST (State [a]) a (Maybe a, Maybe a)
 -- as = input alphabet
 mkGen :: Eq a => [a] -> GEN a 
 mkGen as = S [Start,Wait,Wait] as Start delta finals  -- what is the role of State []?
-    where 
-        delta q (Just a) 
-         | q `elem` [Start,Wait] =
-         [(Wait,[(Just a,Just a)])] ++                               -- not changing anything yet
-         [(Stop,[(Just a, Just b)]) | b <- as, a /= b] ++            -- changing one segment
-         [(Stop,[(Just a, Just a),(Nothing,Just b)]) | b <- as] ++   -- inserting one segment behind the current
-         [(Stop,[(Just a, Nothing )])]                               -- deleting one segment
-        delta Start Nothing = [(Stop,[(Nothing, Just a)]) | a <- as] -- inserting in inital position
-        delta Stop (Just a) =  [(Stop,[(Just a, Just a)])]           -- already done
-        delta q Nothing = [(q, [])]                                  -- do nothing
-        finals = [Start,Stop,Wait]                                   -- not inserting anything
+  where 
+    delta q (Just a) 
+        | q `elem` [Start,Wait] =
+            [(Wait,[(Just a,Just a)])]                             -- not changing anything yet
+         ++ [(Stop,[(Just a, Just b)]) | b <- as, a /= b]          -- changing one segment
+         ++ [(Stop,[(Just a, Just a),(Nothing,Just b)]) | b <- as] -- inserting one segment behind the current
+         ++ [(Stop,[(Just a, Nothing )])]                          -- deleting one segment
+    delta Start Nothing = [(Stop,[(Nothing, Just a)]) | a <- as] -- inserting in inital position
+    delta Stop (Just a) =  [(Stop,[(Just a, Just a)])]           -- already done
+    delta q Nothing = [(q, [])]                                  -- do nothing
+    finals = [Start,Stop,Wait]                                   -- not inserting anything
                                                
 -- CON:
 -- CON is a ranking (=list) of DFSTs that produce lists of violations (True).
@@ -59,26 +59,26 @@ type CON a = [Constraint a]
 -- as = input alphabet, p = which segments must not be deleted?
 mkMax :: Eq a => [a] -> (a -> Bool) -> Constraint a
 mkMax as p = S [Start] (inAlph as) Start delta [Start]
-       where
-              delta _ (Just (Just a, Nothing)) | p a       = return (Start, [True])
-                                               | otherwise = return (Start, mempty) 
-              delta _ _ = return (Start,[])
+  where
+    delta _ (Just (Just a, Nothing)) | p a       = return (Start, [True])
+                                     | otherwise = return (Start, mempty) 
+    delta _ _ = return (Start,[])
 
 -- as = input alphabet, p = which segments must not be inserted?
 mkDep :: Eq a => [a] -> (a -> Bool) -> Constraint a
 mkDep as p = S [Start] (inAlph as) Start delta [Start]
-       where
-              delta _ (Just (Nothing,Just a)) | p a       = return (Start, [True])
-                                              | otherwise = return (Start, mempty) 
-              delta _ _ = return (Start,[])
+  where
+    delta _ (Just (Nothing,Just a)) | p a       = return (Start, [True])
+                                    | otherwise = return (Start, mempty) 
+    delta _ _ = return (Start,[])
 
 -- as = input alphabet, c = if not c a b then give one violation!
 mkId :: Eq a => [a] -> (a -> a -> Bool) -> Constraint a
 mkId as c = S [Start] (inAlph as) Start delta [Start]
-       where
-              delta _ (Just (Just a, Just b)) | c a b     = return (Start, mempty)
-                                              | otherwise = return (Start,[True])
-              delta _ _ = return (Start,[])
+  where
+    delta _ (Just (Just a, Just b)) | c a b     = return (Start, mempty)
+                                    | otherwise = return (Start,[True])
+    delta _ _ = return (Start,[])
 
 -- Markedness constraints:
 -- The bookending Bools mark the boundaries of the string: True = boundary; False = no boundary.
@@ -94,45 +94,47 @@ mkPreds (SQ begin xs end) = SQ begin (map (==) xs) end
 
 mkMark :: Eq a => [a] -> Sequence (a -> Bool) -> Constraint a
 mkMark as (SQ begin sq end)  = S states (inAlph as) Start delta states
-       where
-              states = Start : Wait : steps ++ prefinals
-              prefinals | end = map End $ nub bannedStrings
-                        | otherwise = []
-           -- If abc is banned: 
-           -- steps = [Step [],Step abc,Step ab,Step a] -- Step [] should be removed.
-              steps = nub $ bannedStrings >>= (map Step . inits)
-              bannedStrings = foldr ((<*>) . map (:)) [[]] (zipWith filter sq (repeat as))
-              delta q (Just (_,Nothing)) = Just (q,[]) -- (_) segment was deleted; stay in current state.
-              delta Start (Just (_,Just a)) 
-               | null sq && begin && not end  = Just (Wait,[True])    -- (>) input started; exit start; violation! 
-               | null sq && end               = Just (End [],[True])  -- (<) input could end; go to end; tentative violation? 
-               | [a] `perfectMatch` sq && end = Just (End [a],[True]) -- (>xs<,xs<) xs seen; go to end; tentative violation?
-               | [a] `perfectMatch` sq        = Just (Wait,[True])    -- (>xs,xs) xs seen; wait; violation!
-               | Step [a] `elem` states       = Just (Step [a],[])    -- (>xs,xs,xs<,>xs<) x seen; progress; no violation.
-               | otherwise                    = Just (Wait,[])        -- (_) no x seen; wait; no violation.
-              delta Wait (Just (_,Just a)) 
-               | begin = Just (Wait,[])                               -- (>,>xs,>xs<); wait forever; no violation.
-               | [a] `perfectMatch` sq && end = Just (End [a],[True]) -- (xs<) xs seen; tentative violation?
-               | [a] `perfectMatch` sq        = Just (Wait,[True])    -- (xs) xs seen; violation!
-               | Step [a] `elem` states       = Just (Step [a],[])    -- (xs, xs<) x seen; progress; no violation.
-               | otherwise                    = Just (Wait,[])        -- (_) no x seen; wait; no violation.
-              delta (End xs) (Just (_,Just a)) 
-               | not begin && null sq                     = Just (End xs,[])                        -- (<); input could end; stay in end; already violated.
-               | begin || null (match (drop 1 xs ++ [a])) = Just (Wait,[False])                     -- (><,>xs<,xs<); xs seen, begin or a doesn't belong; wait; cancel violation!
-               | (drop 1 xs ++ [a]) `perfectMatch` sq     = Just (End (drop 1 xs ++ [a]),[])        -- (xs<); xs seen again; stay in end; already violated.
-               | otherwise                                = Just (Step (match $ xs ++ [a]),[False]) -- (xs<); some of xs seen; progress; cancel violation!
-              delta (Step xs) (Just (_,Just a)) 
-               | (xs ++ [a]) `perfectMatch` sq && end          = Just (End (xs ++ [a]),[True])          -- (>xs,>xs<); xs seen; go to end; tentative violation?
-               | (xs ++ [a]) `perfectMatch` sq
-                 && (begin || null (match $ drop 1 xs ++ [a])) = Just (Wait,[True])                     -- (>xs,xs) xs seen, begin or a doesn't belong; wait; violation! 
-               | (xs ++ [a]) `perfectMatch` sq && not begin    = Just (Step (match $ xs ++ [a]),[True]) -- (xs) xs seen, a does belong; progress; violation!
-               | null (match (xs ++ [a])) || begin             = Just (Wait,[])                         -- (>xs,xs,xs<,>xs<) a doesn't belong; wait; no violation.
-               | otherwise                                     = Just (Step (match $ xs ++ [a]),[])     -- (>xs,xs,xs<,>xs<) a does belong; progress; no violation.
-              delta x Nothing = Just (x,[]) -- don't do anything in between inputs!
-              match [] = []
-              match current | Step current `elem` states = current
-                            | otherwise = match $ tail current
-              perfectMatch xs ps = and (zipWith ($) ps xs) && length xs == length ps
+  where
+    states = Start : Wait : steps ++ prefinals
+    prefinals 
+        | end = map End $ nub bannedStrings
+        | otherwise = []
+    -- If abc is banned: 
+    -- steps = [Step [],Step abc,Step ab,Step a] -- Step [] should be removed.
+    steps = nub $ bannedStrings >>= (map Step . inits)
+    bannedStrings = foldr ((<*>) . map (:)) [[]] (zipWith filter sq (repeat as))
+    delta q (Just (_,Nothing)) = Just (q,[]) -- (_) segment was deleted; stay in current state.
+    delta Start (Just (_,Just a)) 
+        | null sq && begin && not end  = Just (Wait,[True])    -- (>) input started; exit start; violation! 
+        | null sq && end               = Just (End [],[True])  -- (<) input could end; go to end; tentative violation? 
+        | [a] `perfectMatch` sq && end = Just (End [a],[True]) -- (>xs<,xs<) xs seen; go to end; tentative violation?
+        | [a] `perfectMatch` sq        = Just (Wait,[True])    -- (>xs,xs) xs seen; wait; violation!
+        | Step [a] `elem` states       = Just (Step [a],[])    -- (>xs,xs,xs<,>xs<) x seen; progress; no violation.
+        | otherwise                    = Just (Wait,[])        -- (_) no x seen; wait; no violation.
+    delta Wait (Just (_,Just a)) 
+        | begin = Just (Wait,[])                               -- (>,>xs,>xs<); wait forever; no violation.
+        | [a] `perfectMatch` sq && end = Just (End [a],[True]) -- (xs<) xs seen; tentative violation?
+        | [a] `perfectMatch` sq        = Just (Wait,[True])    -- (xs) xs seen; violation!
+        | Step [a] `elem` states       = Just (Step [a],[])    -- (xs, xs<) x seen; progress; no violation.
+        | otherwise                    = Just (Wait,[])        -- (_) no x seen; wait; no violation.
+    delta (End xs) (Just (_,Just a)) 
+        | not begin && null sq                     = Just (End xs,[])                        -- (<); input could end; stay in end; already violated.
+        | begin || null (match (drop 1 xs ++ [a])) = Just (Wait,[False])                     -- (><,>xs<,xs<); xs seen, begin or a doesn't belong; wait; cancel violation!
+        | (drop 1 xs ++ [a]) `perfectMatch` sq     = Just (End (drop 1 xs ++ [a]),[])        -- (xs<); xs seen again; stay in end; already violated.
+        | otherwise                                = Just (Step (match $ xs ++ [a]),[False]) -- (xs<); some of xs seen; progress; cancel violation!
+    delta (Step xs) (Just (_,Just a)) 
+        | (xs ++ [a]) `perfectMatch` sq && end          = Just (End (xs ++ [a]),[True])          -- (>xs,>xs<); xs seen; go to end; tentative violation?
+        | (xs ++ [a]) `perfectMatch` sq
+          && (begin || null (match $ drop 1 xs ++ [a])) = Just (Wait,[True])                     -- (>xs,xs) xs seen, begin or a doesn't belong; wait; violation! 
+        | (xs ++ [a]) `perfectMatch` sq && not begin    = Just (Step (match $ xs ++ [a]),[True]) -- (xs) xs seen, a does belong; progress; violation!
+        | null (match (xs ++ [a])) || begin             = Just (Wait,[])                         -- (>xs,xs,xs<,>xs<) a doesn't belong; wait; no violation.
+        | otherwise                                     = Just (Step (match $ xs ++ [a]),[])     -- (>xs,xs,xs<,>xs<) a does belong; progress; no violation.
+    delta x Nothing = Just (x,[]) -- don't do anything in between inputs!
+    match [] = []
+    match current 
+        | Step current `elem` states = current
+        | otherwise = match $ tail current
+    perfectMatch xs ps = and (zipWith ($) ps xs) && length xs == length ps
 
 -- EVAL:
 type Grammar a = (CON a, GEN a)
@@ -140,27 +142,34 @@ type Grammar a = (CON a, GEN a)
 -- outputs an infinite stream of the most harmonic candidates
 cycles :: (Eq a) => Grammar a -> [a] -> [[a]]
 cycles (con, gen) = iterate (mapMaybe snd . bests con . transduce gen)
-       where
-             bests []     candidates = case candidates of
-                                         (x:_) -> x
-                                         _     -> error "some constraints may not be well-defined"
-             bests _     [candidate] = candidate
-             bests (c:cs) candidates = bests cs $ filter (or . fmap ((== bestValue) . balance) . transduce c) candidates
-               where bestValue = minimum $ mapMaybe (fmap balance . transduce c) candidates
-          -- Balance 'cancels' false violations:
-             balance :: [Bool] -> [()] 
-             balance [] = []
-             balance (True:False:bs) = balance bs -- There is no instance for (False:bs) because False should always follow True
-             balance (True:bs)       = ():balance bs 
+  where
+    bests [] candidates = 
+        case candidates of
+            (x:_) -> x
+            _     -> error "some constraints may not be well-defined"
+    bests _ [candidate] = candidate
+    bests (c:cs) candidates = bests cs $ filter (or . fmap ((== bestValue) . balance) . transduce c) candidates
+      where 
+        bestValue = minimum $ mapMaybe (fmap balance . transduce c) candidates
+-- Balance 'cancels' false violations:
+    balance :: [Bool] -> [()] 
+    balance [] = []
+    balance (True:False:bs) = balance bs -- There is no instance for (False:bs) because False should always follow True
+    balance (True:bs)       = ():balance bs 
 
 -- outputs the final most harmonic candidate
 converge :: Eq a => Grammar a -> [a] -> [a]
 converge congen = dupl . cycles congen
-       where dupl (x:y:ys) | x == y = x
-                           | otherwise = dupl (y:ys)
+  where 
+    dupl (x:y:ys) 
+        | x == y    = x
+        | otherwise = dupl (y:ys)
 
 -- outputs the most harmonic candidate at each step
 converge' :: Eq a => Grammar a -> [a] -> [[a]]
 converge' congen = dupl . cycles congen
-       where dupl (x:y:ys) | x == y = [x]
-                           | otherwise = x : dupl (y:ys)
+  where 
+    dupl (x:y:ys) 
+        | x == y    = [x]
+        | otherwise = x : dupl (y:ys)
+ 
